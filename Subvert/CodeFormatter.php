f16d2db760db73efe558dabb0ab3e53798dad4a6
josh <josh@e25faa11-99dc-4a1f-8bbc-1dc317fce81f>
2014-01-22 05:36:52 +0000

    Adding current uncontrolled version.

---
+++ Subvert/CodeFormatter.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Jacere;
+
+require_once(__dir__.'/SyntaxHighlighter.php');
+
+class CodeFormatter {
+	
+	public static function Format($code, $handlers) {
+		
+		$showGutter = false;
+		
+		$code = htmlentities($code, ENT_NOQUOTES);
+		//$code = str_replace("\t", '&#9;', $code);
+		//$code = str_replace(" ", '&nbsp;', $code);
+		
+		if ($handlers) {
+			$code = SyntaxHighlighter::Execute($code, $handlers);
+		}
+		
+		$code_lines = explode("\n", $code);
+		$code_line_numbers = [];
+		foreach ($code_lines as $i => &$line) {
+			if (empty($line)) {
+				$line = '&#8203;';
+			}
+			//$line = sprintf('<div>%1$s</div>', $line);
+			$line = sprintf('<div><code>%1$s</code></div>', $line);
+			//$line = sprintf('%1$s', $line);
+			$code_line_numbers[] = sprintf('<div>%1$s</div>', ($i + 1));
+		}
+		$code = implode('', $code_lines);
+		//$code = implode("\n", $code_lines);
+		
+		$gutter = '';
+		if ($showGutter) {
+			$code_line_numbers_str = implode('', $code_line_numbers);
+			$placeholder = str_repeat('&nbsp;', strlen((string)count($code_lines)));
+			$gutter = <<<EOS
+			<td><div class="gutter">{$code_line_numbers_str}</div><div class="gutter-placeholder">{$placeholder}</div></td>
+EOS;
+		}
+
+		$code = <<<EOS
+		<pre><code><table>
+			<tr>
+				{$gutter}
+				<td><div class="code">{$code}</div></td>
+			</tr>
+		</table></code></pre>
+EOS;
+	
+	return $code;
+	}
+	
+	/*public static function Format($code, $handlers) {
+		
+		$code = htmlentities($code, ENT_NOQUOTES);
+		
+		if ($handlers) {
+			$code = SyntaxHighlighter::Execute($code, $handlers);
+		}
+		
+		$code_lines = explode("\n", $code);
+		$code_line_numbers = [];
+		foreach ($code_lines as $i => &$line) {
+			if (empty($line)) {
+				$line = '&nbsp;';
+			}
+			$line = sprintf('<div>%1$s</div>', $line);
+			$code_line_numbers[] = sprintf('<div>%1$s</div>', ($i + 1));
+		}
+		$code = implode('', $code_lines);
+		
+		$gutter = '';
+		if (true) {
+			$code_line_numbers_str = implode('', $code_line_numbers);
+			$placeholder = str_repeat('&nbsp;', strlen((string)count($code_lines)));
+			$gutter = <<<EOS
+			<td><div class="gutter">{$code_line_numbers_str}</div><div class="gutter-placeholder">{$placeholder}</div></td>
+EOS;
+		}
+
+		$code = <<<EOS
+		<pre><code><table>
+			<tr>
+				{$gutter}
+				<td><div class="code">{$code}</div></td>
+			</tr>
+		</table></code></pre>
+EOS;
+	
+	return $code;
+	}*/
+}
+
+?>
---
+++ Subvert/ReplacementManager.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Jacere;
+
+class ReplacementManager {
+	
+	private $m_map;
+	private $m_replacements;
+	private $m_index;
+	
+	public function __construct() {
+		$this->m_map = [];
+		$this->m_replacements = [];
+		$this->m_index = 0;
+	}
+	
+	public function Replace($text) {
+		$text = strtr($text, $this->m_replacements);
+		$this->m_replacements = [];
+		return $text;
+	}
+	
+	public function Reconstitute($text) {
+		$text = strtr($text, $this->m_map);
+		return $text;
+	}
+	
+	public function Add($str, $replacement, $prefix = NULL) {
+		$code = '';
+		if ($replacement !== '') {
+			$code = "\x1A".'$'.($prefix ? sprintf('_%s_', $prefix) : '').$this->m_index;
+			$this->m_map[$code] = $replacement;
+			$this->m_index++;
+		}
+		$this->m_replacements[$str] = $code;
+	}
+	
+	public function AddReplacedValue($replacement, $prefix = NULL) {
+		
+		// right now duplicate codes are being created for identical replacement values
+		// I don't have a great solution...I might need to break this class into two different approaches.
+		
+		// that's not really what this array is for...this could get confusing
+		/*if (isset($this->m_replacements[$str])) {
+			$code = $this->m_replacements[$str];
+		}
+		else {
+			
+		}*/
+		
+		$code = '';
+		if ($replacement !== '') {
+			$code = "\x1A".'$'.($prefix ? sprintf('_%s_', $prefix) : '').$this->m_index;
+			$this->m_map[$code] = $replacement;
+			$this->m_index++;
+		}
+		return $code;
+	}
+	
+	/*public function AddRegexMatchesBasic2($pattern, $replacement, $text, $handlers = NULL) {
+		$patterns = is_array($pattern) ? $pattern : [$pattern];
+		foreach ($patterns as $p) {
+			if (preg_match_all($p, $text, $matches, PREG_SET_ORDER)) {
+				foreach ($matches as $match) {
+					$match_str = $match[0];
+					
+					if (is_array($handlers)) {
+						$match_str = $this->AddRegexMatches($handlers, $match_str);
+					}
+					
+					// wrap the replacement pattern around each line
+					$match_lines = explode("\n", $match_str);
+					$match_lines_output = [];
+					foreach ($match_lines as $line) {
+						$match_lines_output[] = sprintf($replacement, $line);
+					}
+					$element = implode("\n", $match_lines_output);
+					$this->Add($match[0], $element);
+				}
+				$text = $this->Replace($text);
+			}
+		}
+		return $text;
+	}*/
+	
+	public function AddRegexMatchesBasic($pattern, $replacement, $text, $handlers = NULL) {
+		$text = preg_replace_callback($pattern, function ($match) use ($replacement, $handlers) {
+			$match_str = $match[0];
+			
+			if (is_array($handlers)) {
+				$match_str = $this->AddRegexMatches($handlers, $match_str);
+			}
+			
+			// wrap the replacement pattern around each line
+			$match_lines = explode("\n", $match_str);
+			$match_lines_output = [];
+			foreach ($match_lines as $line) {
+				//$match_lines_output[] = sprintf($replacement, $line);
+				$match_lines_output[] = sprintf('<code class="%s">%s</code>', $replacement, $line);
+			}
+			$element = implode("\n", $match_lines_output);
+			return $this->AddReplacedValue($element);
+		}, $text);
+		return $text;
+	}
+	
+	public function AddRegexMatches(array $handlers, $text) {
+		foreach ($handlers as $key => $handler) {
+			if (is_int($key) || preg_match($key, $text)) {
+				$text = $this->AddRegexMatchesBasic(
+					$handler['pattern'],
+					$handler['wrapper'],
+					$text,
+					(isset($handler['handler']) ? $handler['handler'] : NULL)
+				);
+			}
+		}
+		return $this->Reconstitute($text);
+	}
+}
+
+?>
