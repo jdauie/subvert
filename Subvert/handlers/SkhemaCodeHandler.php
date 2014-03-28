544756637f92669426fc9a84cb73d7d14d3fbb27
josh <josh@e25faa11-99dc-4a1f-8bbc-1dc317fce81f>
2014-03-28 18:20:19 +0000

    Adapting skhema function/filter syntax.

--- Subvert/handlers/SkhemaCodeHandler.php
+++ Subvert/handlers/SkhemaCodeHandler.php
@@ -9,16 +9,17 @@ class SkhemaCodeHandler extends BaseCodeHandler {
 	public function GetPatterns() {
 		return [
 			new CodePattern(
-				'`(?<start>{)(?<type>.)(?<token>[^}]*+)(?<end>})`',
-				['start', 'type', 'token', 'end'],
+				'`(?<start>{)((?<type>[@$#^\.?/])(?<token>[^:}]*+)|(?<evaltype>%))(?<eval>[^}]*+)(?<end>})`',
+				['start', 'type', 'evaltype', 'token', 'eval', 'end'],
 				[
-					NULL    => 'skh-del',
-					'type'  => 'skh-sym',
-					'token' => 'skh-tok',
+					NULL       => 'skh-del',
+					'type'     => 'skh-sym',
+					'evaltype' => 'skh-sym',
+					'token'    => 'skh-tok',
 				],
 				[
-					'token' => new CodePattern(
-						'`(?<delim>:)(?<name>[^[]++)(?<options>\[[^]]*+\])?`',
+					'eval' => new CodePattern(
+						'`(?<delim>:)?(?<name>[^:[]++)(?<options>\[[^]]*+\])?`',
 						['delim', 'name', 'options'],
 						[
 							'delim'   => 'skh-sym',
