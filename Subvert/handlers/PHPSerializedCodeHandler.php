52b48f4473551283a27aa954a0148439151fd8f9
josh <josh@e25faa11-99dc-4a1f-8bbc-1dc317fce81f>
2014-08-07 05:38:59 +0000

    Quotes within strings are not escaped.

--- Subvert/handlers/PHPSerializedCodeHandler.php
+++ Subvert/handlers/PHPSerializedCodeHandler.php
@@ -8,12 +8,14 @@ class PHPSerializedCodeHandler extends BaseCodeHandler {
 	
 	public function GetPatterns() {
 		return [
-			new DelimitedCodePattern(
+			// this doesn't work because strings are not escaped
+			/*new DelimitedCodePattern(
 				SyntaxHighlighter::REGEX_STRING_DOUBLE,
 				'php-str'
-			),
+			),*/
 			new BasicCodePattern(
-				'/[sibaON](?=[:;])/s',
+				//'/[sibaON](?=[:;])/s',
+				'/[sibaON][:](\d+(:|(?=;)))*/s',
 				'keyword'
 			),
 		];
